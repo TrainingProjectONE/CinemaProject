fed94c11eea926055bd62c9431ed670947079603
Bart Benedict <bakosberg@gmail.com>
2023-10-10 20:07:10 +0200

    Test commit git

--- cinema/src/main/java/com/trainingprojectone/cinema/CinemaApplication.java
+++ cinema/src/main/java/com/trainingprojectone/cinema/CinemaApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CinemaApplication {
 
   public static void main(String[] args) {
+
+    System.out.println("Test gita ");
+
     SpringApplication.run(CinemaApplication.class, args);
   }
 
