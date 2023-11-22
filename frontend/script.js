6c456cb3d3a758dde11c66649fe588686d3a5b03
Bart Benedict <bakosberg@gmail.com>
2023-11-22 19:15:45 +0100

    create hall

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,15 @@
 console.log('hallo cinema');
+
+const seats = document.getElementById("seats");
+
+
+
+const seatAmount = 100;
+
+for ( let i = 1; i <= seatAmount; i++) {
+
+    const seatButton = document.createElement("button");
+    seatButton.innerText = i;
+    seats.appendChild(seatButton);
+
+}
